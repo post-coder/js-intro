04cfb0eccfed7c26bad996aa08d94a59ee06ca3a
post-coder <gabrielspanu.dev@gmail.com>
2024-01-25 12:23:37 +0100

    numeri e prompt

--- js/script.js
+++ js/script.js
@@ -1,30 +1,17 @@
-// per dichiarare una variabile, ci serve
-// utilizzare una parola chiave
-// const, let, var
+// const nome = "Silvia";
+
+// document.getElementById("title").innerHTML = "Ciao " + nome + "!";
+
+// // possiamo eseguire la stessa istruzione utilizzando il "template literal",
+// // ovvero
+// document.getElementById("title").innerHTML = `Ciao ${nome}!`;
+
+// // let age = 24;
+
+// // document.getElementById("strong").innerHTML = age;
 
 
+// document.getElementById("strong").innerHTML = prova;
+let pippo = prompt("Come ti chiami?");
 
-// ad esempio: const
-// seguita poi dal nome della variabile
-
-
-const nome = "Federico";
-document.getElementById("title").innerHTML = nome;
-
-
-
-
-// le variabili costanti non possono essere riassegnate
-// in caso ci serva esistono le variabili riassegnabili
-// let
-
-// nessun errore, va tutto bene
-
-
-let cognome;
-cognome = "Saccani";
-cognome = "Palanga";
-
-document.getElementById("paragraph").innerHTML = cognome;
-
-
+document.getElementById("title").innerHTML = `Ciao ${pippo}!`;
