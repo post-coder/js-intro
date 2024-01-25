053d23ac5940e88747e68db10cfb12e700963442
post-coder <gabrielspanu.dev@gmail.com>
2024-01-25 10:42:22 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// per scrivere qualcosa in pagina:
+document.writeln("Prova di foglio di script esterno");
+
+// possiamo anche notificare qualcosa di importante
+// all'utente:
+// alert("Prova");
+
+
+// voglio modificare il titolo della pagina
+
+// mi serve un modo per bersagliarlo
+// gli do quindi un id
+
+// tramite js è possibile ottenere un elemento 
+// del documento attraverso il suo id
+// "document.getElementById("valore)"
+
+// una volta che abbiamo l'elemento, possiamo accedere al suo HTML interno
+// ".innerHTML"
+
+// e possiamo cambiarlo, utilizzando l'assegnazione
+// " = "
+
+document.getElementById("title").innerHTML = "Pagina di Gabriel Spanu";
+
+// modificare il contenuto del secondo titolo
+document.getElementById("dueparole_0").innerHTML = "nuovo contenuto<br>ciao<small>testo piccolino</small>";
+
+// un alert che visualizza il testo dentro il mio titolo
+alert( document.getElementById("title").innerHTML );
