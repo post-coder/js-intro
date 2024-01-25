5310dae8d95df080d14dba0073e104451fff5637
post-coder <gabrielspanu.dev@gmail.com>
2024-01-25 11:52:07 +0100

    variabili

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
-// per scrivere qualcosa in pagina:
-document.writeln("Prova di foglio di script esterno");
-
-// possiamo anche notificare qualcosa di importante
-// all'utente:
-// alert("Prova");
+// per dichiarare una variabile, ci serve
+// utilizzare una parola chiave
+// const, let, var
 
 
-// voglio modificare il titolo della pagina
 
-// mi serve un modo per bersagliarlo
-// gli do quindi un id
+// ad esempio: const
+// seguita poi dal nome della variabile
 
-// tramite js è possibile ottenere un elemento 
-// del documento attraverso il suo id
-// "document.getElementById("valore)"
 
-// una volta che abbiamo l'elemento, possiamo accedere al suo HTML interno
-// ".innerHTML"
+const nome = "Federico";
+document.getElementById("title").innerHTML = nome;
 
-// e possiamo cambiarlo, utilizzando l'assegnazione
-// " = "
 
-// document.getElementById("title").innerHTML = "Pagina di Gabriel Spanu";
 
-// modificare il contenuto del secondo titolo
-document.getElementById("title").innerHTML = "nuovo contenuto<br><small>testo piccolino a capo</small>";
 
-// un alert che visualizza il testo dentro il mio titolo
-alert( document.getElementById("title").innerText);
+// le variabili costanti non possono essere riassegnate
+// in caso ci serva esistono le variabili riassegnabili
+// let
+
+// nessun errore, va tutto bene
+
+
+let cognome;
+cognome = "Saccani";
+cognome = "Palanga";
+
+document.getElementById("paragraph").innerHTML = cognome;
+
+
